7f6908e6a6975863dc59123a383fed6135ecf6f4
OmgVal <ziallshamrock@gmail.com>
2023-08-09 02:16:27 -0700

    notes

---
+++ basics.py
@@ -0,0 +1,9 @@
+#print('OMG IT WORKED', potato)
+
+# line above gives syntax error because the function "potato" was not defined
+
+#python functions start with the 'def' keyword
+def greeting(name):
+    print('hello', name, 'what is up?')
+
+greeting('Chloe')
