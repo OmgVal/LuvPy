03cec2fd0f111dc1b41aef5e22fdc86f832fe183
OmgVal <ziallshamrock@gmail.com>
2023-08-10 00:07:30 -0700

    print elab

--- basics.py
+++ basics.py
@@ -3,11 +3,18 @@
 # line above gives syntax error because the function "potato" was not defined
 
 #python functions start with the 'def' keyword
-def greeting(name):
-    print('hello', name, 'what is up?')
+# def greeting(name):
+#     print('hello', name, 'what is up?')
 
-greeting('Chloe')
+# greeting('Chloe')
 
 #using variables 'message' and giving them a value 'phrase'
-message = 'You are my sunshine...'
-print(message)
+# message = 'You are my sunshine...'
+# print(message)
+
+# this print function print LITERALLYYYY nothing !
+print()
+
+# When to use single/double quotes:
+# use different quotes to help the computer determine when your string begins and end. for example:
+# print('you start here and not "here, but you end" here')
