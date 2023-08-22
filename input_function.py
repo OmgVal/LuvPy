54a19e9b281d2092256a8c64322a8f22b49c222e
OmgVal <ziallshamrock@gmail.com>
2023-08-21 21:52:02 -0700

    getting input!

---
+++ input_function.py
@@ -0,0 +1,9 @@
+# input('what is your name? ')
+
+# the function (input) gets executed first before the string ('hello')
+# print('hello ' + input('what is your name? '))
+
+
+# Exercise !!
+print('Hey ' + input('what is your name? ') + ', how are you?')
+
