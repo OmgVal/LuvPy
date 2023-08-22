324daa4da3b1a107c11497109d810ea8e9c87fa0
OmgVal <ziallshamrock@gmail.com>
2023-08-21 21:27:14 -0700

    new line

--- basics.py
+++ basics.py
@@ -18,3 +18,7 @@ print()
 # When to use single/double quotes:
 # use different quotes to help the computer determine when your string begins and end. for example:
 # print('you start here and not "here, but you end" here')
+
+# New line are made using '\n' a backslash and n
+# print('hello\nhello')
+
