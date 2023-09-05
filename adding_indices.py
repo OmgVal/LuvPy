5b70e26822d88a523486808fdf19cb7cfe113da2
OmgVal <ziallshamrock@gmail.com>
2023-09-04 17:32:11 -0700

    exercise

---
+++ adding_indices.py
@@ -0,0 +1,13 @@
+# Exercise: Get two digit num from user (answer is auto a string), add those two integers together to get the sum using strings index
+
+# my solution:
+# nums = input('give me a 2 digit num:')
+
+# print(int(nums[0]) + int(nums[1]))
+
+# jennys solution: 
+num = input ('enter 2 digit num: ')
+digit1 = num[0]
+digit2 = num[1]
+
+print(int(digit1) + int(digit2))
